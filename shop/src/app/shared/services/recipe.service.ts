5a2e6b937831f5acdd3178ba8e9ff439d25b66d7
Sarath Konuru <sarath.konuru@hitachivantara.com>
2021-12-17 15:31:13 +0530

    added ingredients to recipe model,modified recipe service,updated recipe-detail

--- shop/src/app/shared/services/recipe.service.ts
+++ shop/src/app/shared/services/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from 'src/app/recipes/recipe.model';
+import { Ingredient } from '../ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,20 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("Water","Everybody needs water","https://examsbook.co.in/img/post/large/0lBTMirror-and-Water-Image-Questions.jpg"),
-    new Recipe("H2O","Everybody needs water","https://examsbook.co.in/img/post/large/0lBTMirror-and-Water-Image-Questions.jpg")    
+    new Recipe("Water",
+    "Everybody needs water",
+    "https://examsbook.co.in/img/post/large/0lBTMirror-and-Water-Image-Questions.jpg",
+    [
+      new Ingredient('Hydrogen', 2),
+      new Ingredient('Oxygen', 1)
+    ]),
+    new Recipe("H2O",
+    "Everybody needs water",
+    "https://examsbook.co.in/img/post/large/0lBTMirror-and-Water-Image-Questions.jpg",
+    [
+      new Ingredient('H',2),
+      new Ingredient('O', 2)
+    ])    
   ];
 
   getRecipes() {
