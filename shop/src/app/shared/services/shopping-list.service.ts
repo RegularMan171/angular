49454b38aa69871da714c14963beb02b0e5cac36
Sarath Konuru <sarath.konuru@hitachivantara.com>
2021-12-17 15:07:38 +0530

    shopping-list service added wherever required

--- shop/src/app/shared/services/shopping-list.service.ts
+++ shop/src/app/shared/services/shopping-list.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
 
+  private ingredients: Ingredient[] = [
+    new Ingredient("Hydrogen", 2),
+    new Ingredient("Oxygen", 1)
+  ];
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
   constructor() { }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(i: Ingredient) {
+    this.ingredients.push(i);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
--- shop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shop/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from 'src/app/shared/services/shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,15 +11,13 @@ export class ShoppingEditComponent implements OnInit {
 
   @ViewChild('nameInput', {static:true}) nameInputElem: ElementRef;
   @ViewChild('amountInput', {static:true}) amountInputElem: ElementRef;
-  constructor() { }
-
-  @Output() ingredient = new EventEmitter<Ingredient>();
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit(): void {
   }
   onAddIngredient() {
     const newIngredient = new Ingredient(this.nameInputElem.nativeElement.value, this.amountInputElem.nativeElement.value);
-    this.ingredient.emit(newIngredient)
+    this.sls.addIngredient(newIngredient);
   }
 
 }
