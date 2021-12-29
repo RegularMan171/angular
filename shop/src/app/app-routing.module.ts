62b53165bb3f7cc59c7e952bf89c6ab8d2eaa6f3
Sarath Konuru <sarath.konuru@hitachivantara.com>
2021-12-29 11:39:33 +0530

    added navigation

--- shop/src/app/app-routing.module.ts
+++ shop/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/recipes', pathMatch: 'full'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'slist', component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
