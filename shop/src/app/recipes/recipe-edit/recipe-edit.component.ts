4f06fc83c1c31954b2e1ba46eca1739a939b4259
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-01-24 15:31:44 +0530

    added delete all ingredients functionality

--- shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -97,4 +97,8 @@ export class RecipeEditComponent implements OnInit {
     (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
   }
 
+  onDeleteAllIngredients() {
+    (<FormArray>this.recipeForm.get('ingredients')).clear();
+  }
+
 }
