5aa092f04c9f959b99511fbe59c10c5408d49250
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-01-24 13:14:42 +0530

    added recipe form

--- shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ shop/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { RecipeService } from 'src/app/shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,15 +12,62 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
-  constructor(private route: ActivatedRoute) { }
+  recipeForm: FormGroup;
+
+  // get controls() { // a getter!
+  //   return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  // }
+
+  constructor(private route: ActivatedRoute, private rs: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     );
   }
+
+  private initForm() {
+
+    let recipeName='';
+    let recipeImgPath = '';
+    let recipeDesc = '';
+    let recipeIngredients = new FormArray([]);
+
+    if(this.editMode) {
+      const recipe = this.rs.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImgPath = recipe.imagePath;
+      recipeDesc = recipe.description;
+      if(recipe['ingredients']) {
+        for(let ing of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ing.name),
+              'amount': new FormControl(ing.amount)
+            })
+          );
+        }
+      }
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imgPath': new FormControl(recipeImgPath),
+      'description': new FormControl(recipeDesc)
+    });
+  }
+
+  onSubmit() {
+  console.log(this.recipeForm.value)
+  }
+
+    get controls() { // a getter!
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
 
 }
